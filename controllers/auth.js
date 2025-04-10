55fdd33fae1cd401f543e65563326f5ce160e686
Aameer <officialaameer001@gmail.com>
2025-04-10 21:23:22 +0530

    Created some users, implemented password hashing and created jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,22 @@
-const register = (req, res) => {
-  return res.status(201).json({ msg: "User Registered Successfully" });
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = await User.create({ name, email, password: hashedPassword });
+  const token = jwt.sign(
+    { userId: newUser._id, userName: newUser.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+  return res.status(201).json({ user: { name: newUser.name }, token });
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
   return res.status(201).json({ msg: "User Logged in" });
 };
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const stricterEmailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -7,4 +9,19 @@ const userSchema = new mongoose.Schema({
     minLength: 3,
     maxLength: 50,
   },
+
+  email: {
+    type: String,
+    required: [true, "Email Id must be provided"],
+    match: [stricterEmailRegex, "Please provide valid email address"],
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: [true, "password must be provided"],
+    minLength: 4,
+  },
 });
+
+module.exports = mongoose.model("User", userSchema);
