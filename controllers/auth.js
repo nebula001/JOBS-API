cf3b376cc83b130305122ad1a927739e4cbbbb3e
Aameer <officialaameer001@gmail.com>
2025-04-13 02:05:57 +0530

    Implemented User Registration with jwt generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,10 @@
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const newUser = await User.create({ name, email, password: hashedPassword });
-  const token = jwt.sign(
-    { userId: newUser._id, userName: newUser.name },
-    process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_LIFETIME }
-  );
+  const newUser = await User.create({ name, email, password });
+  const token = newUser.getJWT();
   return res.status(201).json({ user: { name: newUser.name }, token });
 };
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const stricterEmailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -23,5 +27,19 @@ const userSchema = new mongoose.Schema({
     minLength: 4,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.getJWT = function () {
+  return jwt.sign(
+    { userId: this._id, userName: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
 
 module.exports = mongoose.model("User", userSchema);
